f7225f16f3c97a02e858f15caaa45a977220f4f7
Jinghao Jia <jiajinghao1998@gmail.com>
2018-03-28 10:24:05 +0900

    Indentation

--- BiggerIsGreater.java
+++ BiggerIsGreater.java
@@ -1,54 +1,54 @@
 /* Always look for the increasing string starts from the end
-   Then switch the char right before the string with the minimum char
-   that is greater than it in the increasing string. Finally, sort the
-   modified increasing string.
- */
+Then switch the char right before the string with the minimum char
+that is greater than it in the increasing string. Finally, sort the
+modified increasing string.
+*/
 import java.io.*;
 import java.util.*;
 
 
 public class Solution {
 
-    private static String noSol = "no answer";
+  private static String noSol = "no answer";
 
-    public static void main(String[] args) {
-        boolean possible = false;
+  public static void main(String[] args) {
+    boolean possible = false;
 
-        //Scanner to read input;
-        Scanner input = new Scanner(System.in);
+    //Scanner to read input;
+    Scanner input = new Scanner(System.in);
 
-        //Number of queries
-        long strNum = input.nextInt();
-        long num = 0;
-        while(num++ < strNum) {
-            possible = false;
-            char charArr[] = input.next().toCharArray();
-            int count = 1;
-            char pre = charArr[charArr.length - 1];
-            int i;
-            for(i = charArr.length - 2; i >= 0; i--) {
-                if(charArr[i] >= pre)
-                    count++;
-                else
-                    break;
-                pre = charArr[i];
-            }
-            if(count == charArr.length) {
-                System.out.println(noSol);
-                continue;
-            }
-            for(int j = charArr.length - 1; j >= 0; j--) {
-                if(charArr[j] > charArr[i]) {
-                    char temp = charArr[j];
-                    charArr[j] = charArr[i];
-                    charArr[i] = temp;
-                    Arrays.sort(charArr, i + 1, charArr.length);
-                    break;
-                }
-            }
-            System.out.println(new String(charArr));
-
+    //Number of queries
+    long strNum = input.nextInt();
+    long num = 0;
+    while(num++ < strNum) {
+      possible = false;
+      char charArr[] = input.next().toCharArray();
+      int count = 1;
+      char pre = charArr[charArr.length - 1];
+      int i;
+      for(i = charArr.length - 2; i >= 0; i--) {
+        if(charArr[i] >= pre)
+        count++;
+        else
+        break;
+        pre = charArr[i];
+      }
+      if(count == charArr.length) {
+        System.out.println(noSol);
+        continue;
+      }
+      for(int j = charArr.length - 1; j >= 0; j--) {
+        if(charArr[j] > charArr[i]) {
+          char temp = charArr[j];
+          charArr[j] = charArr[i];
+          charArr[i] = temp;
+          Arrays.sort(charArr, i + 1, charArr.length);
+          break;
         }
+      }
+      System.out.println(new String(charArr));
 
     }
+
+  }
 }
--- GridSearch.java
+++ GridSearch.java
@@ -7,60 +7,60 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            boolean found = false;
-            int R = in.nextInt();
-            int C = in.nextInt();
-            String[] G = new String[R];
-            for(int G_i=0; G_i < R; G_i++){
-                G[G_i] = in.next();
-            }
-            int r = in.nextInt();
-            int c = in.nextInt();
-            String[] P = new String[r];
-            for(int P_i=0; P_i < r; P_i++){
-                P[P_i] = in.next();
-            }
-            //Nested loop to traverse through the whole matrix
-            for(int i = 0; i < R; i++) {
-                for(int j = 0; j < C; j++) {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int t = in.nextInt();
+    for(int a0 = 0; a0 < t; a0++){
+      boolean found = false;
+      int R = in.nextInt();
+      int C = in.nextInt();
+      String[] G = new String[R];
+      for(int G_i=0; G_i < R; G_i++){
+        G[G_i] = in.next();
+      }
+      int r = in.nextInt();
+      int c = in.nextInt();
+      String[] P = new String[r];
+      for(int P_i=0; P_i < r; P_i++){
+        P[P_i] = in.next();
+      }
+      //Nested loop to traverse through the whole matrix
+      for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
 
-                    //If the first char matches and the matrix is still in bound
-                    if(G[i].charAt(j) == P[0].charAt(0) && i + r <= R && j + c <= C) {
-                        boolean fail = false; //Flag to indicator whether this is a successful match
+          //If the first char matches and the matrix is still in bound
+          if(G[i].charAt(j) == P[0].charAt(0) && i + r <= R && j + c <= C) {
+            boolean fail = false; //Flag to indicator whether this is a successful match
 
-                        //Loop to check the whole possible area
-                        for(int ii = 0; ii < r; ii++) {
+            //Loop to check the whole possible area
+            for(int ii = 0; ii < r; ii++) {
 
-                            //If not success, stop
-                            if(fail)
-                                break;
-                            for(int jj = 0; jj < c; jj++) {
+              //If not success, stop
+              if(fail)
+              break;
+              for(int jj = 0; jj < c; jj++) {
 
-                                // If fails to match one element, stop and set flag
-                                if(G[i + ii].charAt(j + jj) != P[ii].charAt(jj)) {
-                                    fail = true;
-                                    break;
-                                }
-                                if(ii == r - 1 && jj == c - 1) {
-                                    found = true;
-                                }
-                            }
-                        }
-                    }
-                    if(found)
-                        break;
+                // If fails to match one element, stop and set flag
+                if(G[i + ii].charAt(j + jj) != P[ii].charAt(jj)) {
+                  fail = true;
+                  break;
                 }
-                if(found)
-                    break;
+                if(ii == r - 1 && jj == c - 1) {
+                  found = true;
+                }
+              }
             }
-            if(found)
-                System.out.println("YES");
-            else
-                System.out.println("NO");
+          }
+          if(found)
+          break;
         }
+        if(found)
+        break;
+      }
+      if(found)
+      System.out.println("YES");
+      else
+      System.out.println("NO");
     }
+  }
 }
